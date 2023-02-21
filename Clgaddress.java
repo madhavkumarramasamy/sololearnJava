ba66ef163e25e3369b0378e794334b5520167c98
madhavkumarramasamy <125901946+madhavkumarramasamy@users.noreply.github.com>
2023-02-21 16:22:21 +0530

    Program clgaddress

---
+++ Clgaddress.java
@@ -0,0 +1,7 @@
+public class CollegeAddress {
+    public static void main(String args[]) {
+      System.out.println("Government College of Engineering");
+      System.out.println("NH 44 ,Karuppur");
+      System.out.println(" Salem-636011");
+    }
+}
