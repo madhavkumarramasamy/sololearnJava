55c9a8614d4072de3e2d2a2e2afb39d5a1ee7766
madhavkumarramasamy <125901946+madhavkumarramasamy@users.noreply.github.com>
2023-02-21 16:12:55 +0530

    Program Friend

---
+++ Friend.java
@@ -0,0 +1,9 @@
+public class Friend {
+    public static void main(String args[]) {
+      System.out.println("Karthik");
+      System.out.println("Moorthy");
+      System.out.println("Selvavairavan");
+      System.out.println("Mohanraj");
+      System.out.println("Govarthan");
+    }
+}
