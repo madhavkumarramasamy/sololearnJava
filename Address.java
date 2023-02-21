eb9de724ab828557af03185a773cbff8ae3cf4eb
madhavkumarramasamy <125901946+madhavkumarramasamy@users.noreply.github.com>
2023-02-21 16:18:59 +0530

    Program Address

---
+++ Address.java
@@ -0,0 +1,9 @@
+public class HomeAddress {
+    public static void main(String args[]) {
+      System.out.println("A2 Harini Apartment");
+      System.out.println("Saral Village");
+      System.out.println("Nagaramalai Adivaram");
+      System.out.println("Alagapuram Periyaputhur");
+      System.out.println("Salem");
+    }
+}
