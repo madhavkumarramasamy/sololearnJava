70a5fcc0b4aa6bd813923d7b652568df49f1edc5
madhavkumarramasamy <125901946+madhavkumarramasamy@users.noreply.github.com>
2023-03-01 18:56:02 +0530

    program circle

---
+++ circle.java
@@ -0,0 +1,14 @@
+public class circle {
+    public static void main(String args[]) 
+    {
+    circle c=new circle();
+        
+         c.circle1();
+    }
+    public void circle1()
+    {
+      int r=4;
+      double a=3.14*(r*r);
+      System.out.println(a);
+    }
+}
