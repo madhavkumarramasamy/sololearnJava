1a5505f8a069208dae4504b8f73ab955673a2c2c
madhavkumarramasamy <125901946+madhavkumarramasamy@users.noreply.github.com>
2023-03-01 18:33:17 +0530

    program triangle

---
+++ triangle.java
@@ -0,0 +1,15 @@
+public class triangle {
+    public static void main(String args[]) 
+    {
+    triangle t=new triangle();
+        
+         t.triangle1();
+    }
+    public void triangle1()
+    {
+      int h=12;
+      int b=6;
+      int a=(b*h)/2;
+      System.out.println(a);
+    }
+}
