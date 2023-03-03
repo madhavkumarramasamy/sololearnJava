9db44286dd7a8ffb80d0b5acf25b26baff4b91f4
madhavkumarramasamy <125901946+madhavkumarramasamy@users.noreply.github.com>
2023-03-03 15:18:54 +0530

    program square

---
+++ square.java
@@ -0,0 +1,21 @@
+//Category -3
+//return type fn()
+
+public class square {
+   
+   
+     public static void main(String args[])
+    {
+      square o=new square();
+      int result=o.square();
+      System.out.println(result);
+    }
+
+    
+    public int square()
+    {
+      int s=9;
+      int a=s*s;
+      return a;
+    }
+}
