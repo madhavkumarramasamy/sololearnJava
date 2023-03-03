39dd6689934e8ff74be87733d94db57fb3fd42ef
madhavkumarramasamy <125901946+madhavkumarramasamy@users.noreply.github.com>
2023-03-03 15:20:35 +0530

    program rect

---
+++ rec.java
@@ -0,0 +1,22 @@
+//Category -3
+//return type fn()
+
+public class rectangle {
+   
+   
+     public static void main(String args[])
+    {
+      rectangle o=new rectangle();
+      int result=o.rectangle();
+      System.out.println(result);
+    }
+
+    
+    public int rectangle()
+    {
+      int l=9;
+      int w=5;
+      int a=l*w;
+      return a;
+    }
+}
