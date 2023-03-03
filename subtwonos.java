69cf6b34782a3338c059e45bf2bf436a290c808c
madhavkumarramasamy <125901946+madhavkumarramasamy@users.noreply.github.com>
2023-03-03 15:16:49 +0530

    program subtwonos

---
+++ subtwonos.java
@@ -0,0 +1,22 @@
+//Category -3
+//return type fn()
+
+public class MyClass {
+   
+   
+     public static void main(String args[])
+    {
+      MyClass o=new MyClass();
+      int result=o.sub();
+      System.out.println(result);
+    }
+
+    
+    public int sub()
+    {
+      int x=90;
+      int y=60;
+      int z=x-y;
+      return z;
+    }
+}
