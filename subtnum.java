8255f2cef582ca1400bf36cf884a30770bb2cb6d
madhavkumarramasamy <125901946+madhavkumarramasamy@users.noreply.github.com>
2023-03-04 21:17:23 +0530

    program subtnum

---
+++ subtnum.java
@@ -0,0 +1,21 @@
+//Category -4
+//return type fn(args)
+
+public class MyClass {
+   
+   
+     public static void main(String args[])
+    {
+      MyClass o=new MyClass();
+      int x=150,y=90;
+      int result=o.sub(x,y);
+      System.out.println(result);
+    }
+
+    
+    public int sub(int a,int b)
+    {
+      int z=a-b;
+      return z;
+    }
+}
